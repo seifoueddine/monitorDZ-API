d5afce6fe53d835762b79f196f98e1173601ebbb
seif eddine <nouaraseifeddine@gmail.com>
2020-08-01 13:52:01 +0100

    create sector CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   namespace :api do
@@ -12,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :slugs
+      resources :sectors
     end
    end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_233945) do
+ActiveRecord::Schema.define(version: 2020_08_01_124500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sectors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
