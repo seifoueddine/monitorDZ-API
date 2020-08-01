2a89c9d6967b640113fe15fe2e86cdedd2d3b7d7
seif eddine <nouaraseifeddine@gmail.com>
2020-08-01 15:54:28 +0100

    add sectors to media json

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -13,13 +13,14 @@ class Api::V1::MediaController < ApplicationController
                            ])
     end
     set_pagination_headers :media
-    json_string = MediaSerializer.new(@media).serialized_json
+    json_string = MediaSerializer.new(@media, include: [:sectors]).serialized_json
     render  json: json_string
   end
 
   # GET /media/1
   def show
-    render json: @medium
+    json_string = MediaSerializer.new(@medium, include: [:sectors]).serialized_json
+    render  json: json_string
   end
 
   # POST /media
--- app/serializers/media_serializer.rb
+++ app/serializers/media_serializer.rb
@@ -1,4 +1,6 @@
 class MediaSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :type, :orientation
+  has_many :media_sectors
+  has_many :sectors, through: :media_sectors
 end
