02d2abcc61c5de37f432181f523eb34d85f8496b
seif eddine <nouaraseifeddine@gmail.com>
2020-08-05 14:08:47 +0100

    change seralizer campaigns relations

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -2,7 +2,7 @@ class CampaignSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :start_date, :end_date
   has_many :campaign_media
-  has_many :campaigns, through: :campaign_media
+  has_many :media, through: :campaign_media
   has_many :campaign_sectors
-  has_many :campaigns, through: :campaign_sectors
+  has_many :sectors, through: :campaign_sectors
 end
