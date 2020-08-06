f83d2f4532ecd6e251a727b2182014b9f1d071c2
seif eddine <nouaraseifeddine@gmail.com>
2020-08-06 16:13:58 +0100

    function to baned remove

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -3,4 +3,19 @@ class Sector < ApplicationRecord
     has_many :media, through: :media_sectors
     has_many :campaign_sectors
     has_many :campaigns, through: :campaign_sectors
+
+    before_destroy :check
+
+def check
+  status = true
+  if self.media_sectors.count > 0
+    self.errors[:deletion_status] = 'Cannot delete sector with active media in it.'
+    status = false
+  else
+    self.errors[:deletion_status] = 'OK.'
+  end
+  status
+end
+
+
 end
