54679e29f91aba34d7ac4b231186cedbba3c00a7
seif eddine <nouaraseifeddine@gmail.com>
2020-08-19 23:49:44 +0100

    add relation between tags and articles

---
+++ db/migrate/20200819224146_create_article_tags.rb
@@ -0,0 +1,10 @@
+class CreateArticleTags < ActiveRecord::Migration[6.0]
+  def change
+    create_table :article_tags do |t|
+      t.integer :tag_id
+      t.integer :article_id
+
+      t.timestamps
+    end
+  end
+end
