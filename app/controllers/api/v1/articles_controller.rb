78aaa8ab5ad082c633aaf23e0fb601fdf0efe86f
seif eddine <nouaraseifeddine@gmail.com>
2020-09-05 10:52:07 +0100

    fix articles search

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -102,7 +102,7 @@ class Api::V1::ArticlesController < ApplicationController
     set_pagination_headers :articles_res
     json_string = ArticleSerializer.new(@articles_res).serialized_json
 
-    render json: { result_articles: json_string, time: result_articles.took }
+    render json: { result_articles: result_articles, time: result_articles.took }
 
   end
 
