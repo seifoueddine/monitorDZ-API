05cd0c41bf17578655253482c47d4a340e141bf7
seif eddine <nouaraseifeddine@gmail.com>
2020-09-12 13:55:35 +0100

    add URI to ennahar

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -344,7 +344,7 @@ class Api::V1::ArticlesController < ApplicationController
     articles_url_ennahar = []
     last_dates = []
     url_media_array.map do |url|
-      doc = Nokogiri::HTML(open(url, 'User-Agent' => 'opera'))
+      doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'opera'))
       doc.css('div.article__image.article__image--medium a').map do |link|
         articles_url_ennahar << link['href']
       end
@@ -362,7 +362,7 @@ class Api::V1::ArticlesController < ApplicationController
     end
     articles_url_ennahar_after_check = articles_url_ennahar - list_articles_url
     articles_url_ennahar_after_check.map do |link|
-      article = Nokogiri::HTML(open(link, 'User-Agent' => 'opera'))
+      article = Nokogiri::HTML(URI.open(link, 'User-Agent' => 'opera'))
       new_article = Article.new
       new_article.url_article = link
       new_article.medium_id = @media.id
