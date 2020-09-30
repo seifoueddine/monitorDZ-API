97112f1188ec24f0af65c8e165b9e6a1da9119d4
seif eddine <nouaraseifeddine@gmail.com>
2020-09-30 08:58:15 +0100

    add elasticsearch url

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 if Rails.env == 'production'
-  url = ENV["BONSAI_URL"]
-    transport_options = { request: { timeout: 250 } }
-    options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
-    Searchkick.client = Elasticsearch::Client.new(options)
-  end
+  url = ENV['BONSAI_URL']
+  transport_options = { request: { timeout: 250 } }
+  options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+  Searchkick.client = Elasticsearch::Client.new(options)
+  ENV['ELASTICSEARCH_URL'] = 'http://seifou:123456@161.97.114.39/:81'
+end
