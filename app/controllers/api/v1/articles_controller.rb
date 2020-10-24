5bad7b78b30a38b056a19786af636caa4ec93c25
seif eddine <nouaraseifeddine@gmail.com>
2020-10-24 07:59:19 +0100

    fix html to pdf action

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -160,7 +160,7 @@ class Api::V1::ArticlesController < ApplicationController
     id = params[:id]
     @article = Article.find(id)
     @html = get_html
-    pdf = WickedPdf.new.pdf_from_string('<h1>Seifou eddine NOUARA </h1>')
+    pdf = WickedPdf.new.pdf_from_string('<h1>' + @article.title + '</h1>')
     send_data pdf, filename: 'file.pdf'
   end
 
