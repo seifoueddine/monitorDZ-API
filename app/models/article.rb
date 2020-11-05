943d8e0bc48624cb5b871ad0eb2d6acc50f141bc
seif eddine <nouaraseifeddine@gmail.com>
2020-11-05 22:48:30 +0100

    add new mapping

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,134 @@
 class Article < ApplicationRecord
   searchkick match: :word_middle,
-            suggest: %i[title body media_area medium_type author_name tag_name]
+            suggest: %i[title body media_area medium_type author_name tag_name],
+             "mapping": {
+                 "article": {
+                     "dynamic_templates": [
+                         {
+                             "string_template": {
+                                 "match": "*",
+                                 "match_mapping_type": "string",
+                                 "mapping": {
+                                     "fields": {
+                                         "word_middle": {
+                                             "analyzer": "searchkick_word_middle_index",
+                                             "index": true,
+                                             "type": "text"
+                                         }
+                                     },
+                                     "ignore_above": 30000,
+                                     "type": "keyword"
+                                 }
+                             }
+                         }
+                     ],
+                     "properties": {
+                         "author_id": {
+                             "type": "long"
+                         },
+                         "author_name": {
+                             "type": "keyword",
+                             "fields": {
+                                 "suggest": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_suggest_index"
+                                 },
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         },
+                         "body": {
+                             "type": "text",
+                             "fields": {
+                                 "suggest": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_suggest_index"
+                                 },
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         },
+                         "date_published": {
+                             "type": "date"
+                         },
+                         "language": {
+                             "type": "keyword",
+                             "fields": {
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         },
+                         "media_area": {
+                             "type": "keyword",
+                             "fields": {
+                                 "suggest": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_suggest_index"
+                                 },
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         },
+                         "medium_id": {
+                             "type": "long"
+                         },
+                         "medium_type": {
+                             "type": "keyword",
+                             "fields": {
+                                 "suggest": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_suggest_index"
+                                 },
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         },
+                         "tag_name": {
+                             "type": "keyword",
+                             "fields": {
+                                 "suggest": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_suggest_index"
+                                 },
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         },
+                         "title": {
+                             "type": "keyword",
+                             "fields": {
+                                 "suggest": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_suggest_index"
+                                 },
+                                 "word_middle": {
+                                     "type": "text",
+                                     "analyzer": "searchkick_word_middle_index"
+                                 }
+                             },
+                             "ignore_above": 30000
+                         }
+                     }
+                 }
+             }
 
 
 =begin
