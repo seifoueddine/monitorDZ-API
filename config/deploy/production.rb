f7100addbde48bbcde751fb5e3fe0f0bcf91d113
seif eddine <nouaraseifeddine@gmail.com>
2021-05-11 13:45:02 +0100

    change user for deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '161.97.114.39', user: 'root', roles: %w{app db web}
+server '161.97.114.39', user: 'seifou', roles: %w{app db web}
 
 # role-based syntax
 # ==================
