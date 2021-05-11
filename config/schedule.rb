ce9a5bdba9dd7ff65116cb020b9330aaa6a92853
seif eddine <nouaraseifeddine@gmail.com>
2021-05-11 14:07:32 +0100

    add log cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,7 +5,7 @@
 
 # Example:
 #
-# set :output, "/path/to/my/cron_log.log"
+
 #
 # every 2.hours do
 #   command "/usr/bin/some_great_command"
@@ -13,14 +13,16 @@
 #   rake "some:great:rake:task"
 # end
 
-every 10.minutes do
-   runner 'Article.reindex()'
-end
+# every 10.minutes do
+#    runner 'Article.reindex()'
+# end
 
 every :day, at: '8:00am,11:00am,1:00pm,4:00pm,7:00pm,10:00pm', roles: [:app, :web, :db] do
    rake 'crawling:scraping' 
  end
- every 10.minutes,  roles: [:app, :web, :db] do
+ 
+ set :output, "/var/log/nginx/cron_log.log"
+every 10.minutes,  roles: [:app, :web, :db] do
    rake 'crawling:scraping' 
  end
 
