300e29fb2fc84513cb38244bb0283a37cf84c1f3
seif eddine <nouaraseifeddine@gmail.com>
2021-05-11 17:19:56 +0100

    add config

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,19 @@ set :keep_releases, 5
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
 
 
+append :linked_files, "config/master.key"
 
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f /var/www/html/#{fetch :application}/config/master.key ]")
+          upload! 'config/master.key', "/var/www/html/#{fetch :application}/config/master.key"
+        end
+      end
+    end
+  end
+end
 
 
 
