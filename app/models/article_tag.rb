8d27b3a2d1fde2363610d8d6c2d9106692c78a30
seif eddine <nouaraseifeddine@gmail.com>
2021-06-15 12:50:18 +0100

    add reindex tag

--- app/models/article_tag.rb
+++ app/models/article_tag.rb
@@ -1,4 +1,9 @@
 class ArticleTag < ApplicationRecord
   belongs_to :article
   belongs_to :tag
+  after_commit :reindex_article
+
+  def reindex_article
+    Article.reindex
+  end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,9 @@ class Tag < ApplicationRecord
   has_many :articles, through: :article_tags
   has_many :campaign_tags
   has_many :campaigns, through: :campaign_tags
+  after_commit :reindex_article
+
+  def reindex_article
+    Article.reindex
+  end
 end
