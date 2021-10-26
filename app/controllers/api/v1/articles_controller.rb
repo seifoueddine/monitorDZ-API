3cadeb803d7909e7f4088e474518b772c4276e96
Seif Eddine.N <nouaraseifeddine@gmail.com>
2021-10-26 20:24:53 +0100

    fix update article

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -230,11 +230,6 @@ class Api::V1::ArticlesController < ApplicationController
   # PATCH/PUT /articles/1
   def update
     if @article.update(article_params)
-      if params[:author_id]
-        @article.author.clear
-        author = Author.where(id: params[:author_id])
-        @article.author = author
-      end
       json_string = ArticleSerializer.new(@article).serializable_hash.to_json
       render json: json_string
     else
