83fd1e9a5080bc90445dffd94a3c129674f17cb3
Seif Eddine.N <nouaraseifeddine@gmail.com>
2021-10-27 11:35:01 +0100

    test

--- app/controllers/api/v1/authors_controller.rb
+++ app/controllers/api/v1/authors_controller.rb
@@ -17,7 +17,9 @@ class Api::V1::AuthorsController < ApplicationController
     # set_pagination_headers :authors
     # json_string = AuthorSerializer.new(@authors).serializable_hash.to_json
     @authors.map do |author|
-      author[0].merge!(articles_count: author[0].articles.count)
+      puts author
+      puts author.name
+      # author.merge!(articles_count: author.articles.count)
     end
     render json: @authors
   end
