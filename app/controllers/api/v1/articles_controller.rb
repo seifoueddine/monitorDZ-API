5b2edec94629ea39ceed05f1fcb5f36315c92d39
Seif Eddine.N <nouaraseifeddine@gmail.com>
2021-11-04 21:42:47 +0100

    add new media experssion

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -694,7 +694,7 @@ div.nobreak { page-break-inside: avoid; }
     when 'MAGHREBEMERGENT'
       get_articles_maghrebemergent(url_media_array)
       when 'ELBILAD'
-      get_articles_bilad(url_media_array)
+        get_articles_bilad(url_media_array)
     when 'ELMOUDJAHID'
       get_articles_elmoudjahid(url_media_array)
     when 'ELMOUDJAHID-FR'
@@ -719,7 +719,7 @@ div.nobreak { page-break-inside: avoid; }
       get_articles_santenews(url_media_array)
     when 'ALGERIE360'
       get_articles_algerie360(url_media_array)
-      when 'ALGERIEPARTPLUS'
+    when 'ALGERIEPARTPLUS'
       get_articles_algerie_part(url_media_array)
     when '24H-DZ'
       get_articles_24hdz(url_media_array)
@@ -727,6 +727,8 @@ div.nobreak { page-break-inside: avoid; }
       get_articles_reporters(url_media_array)
     when 'SHIHABPRESSE'
       get_articles_shihabpresse(url_media_array)
+    when 'LEXPRESSIONDZ'
+      get_articles_lexpressiondz(url_media_array)
     else
       render json: { crawling_status: 'No media name found!! ', status: 'error' }
     end
@@ -1100,7 +1102,7 @@ div.nobreak { page-break-inside: avoid; }
              new_article.image = Down.download(url_array[0])
            end
       rescue Down::Error => e
-           puts "Can't download this image #{url_array[0]}"
+        puts "Can't download this image #{url_array[0]}"
            puts e.message
            puts
            new_article.image = nil
@@ -1675,7 +1677,7 @@ div.nobreak { page-break-inside: avoid; }
              new_article.image = Down.download(url_array[0])
            end
       rescue Down::Error => e
-           puts "Can't download this image #{url_array[0]}"
+        puts "Can't download this image #{url_array[0]}"
            puts e.message
            puts
            new_article.image = nil
@@ -1690,7 +1692,7 @@ div.nobreak { page-break-inside: avoid; }
   end
   # end method to get bilad articles
   #
-  #
+
 
   # start method to get maghrebemergent articles
   def get_articles_maghrebemergent(url_media_array)
@@ -1995,7 +1997,7 @@ div.nobreak { page-break-inside: avoid; }
              new_article.image = Down.download(url_array[0])
            end
       rescue Down::Error => e
-           puts "Can't download this image #{url_array[0]}"
+        puts "Can't download this image #{url_array[0]}"
            puts e.message
            puts
            new_article.image = nil
@@ -2910,7 +2912,7 @@ div.nobreak { page-break-inside: avoid; }
              new_article.image = Down.download(url_array[0])
            end
       rescue Down::Error => e
-           puts "Can't download this image #{url_array[0]}"
+        puts "Can't download this image #{url_array[0]}"
            puts e.message
            puts
            new_article.image = nil
@@ -3025,6 +3027,99 @@ div.nobreak { page-break-inside: avoid; }
     render json: { crawling_count_shihabpresse: count }
   end
   # end method to get shihabpresse articles
+
+
+
+  # start method to get expression articles
+  def get_articles_lexpressiondz(url_media_array)
+    articles_url_lexpressiondz = []
+    url_media_array.map do |url|
+      begin
+        doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+      rescue OpenURI::HTTPError => e
+        puts "Can't access #{url}"
+        puts e.message
+        puts
+        next
+      end
+
+      doc.css('article h2 a').map do |link|
+        articles_url_lexpressiondz << link['href']
+      end
+    end
+    articles_url_lexpressiondz = articles_url_lexpressiondz.reject(&:nil?)
+
+    articles_url_lexpressiondz_after_check = []
+    articles_url_lexpressiondz.map do |link|
+      articles_url_lexpressiondz_after_check << link unless Article.where(medium_id: @media.id,url_article: link).present?
+    end
+
+    articles_url_lexpressiondz_after_check.map do |link|
+
+
+      begin
+        article = Nokogiri::HTML(open(link, 'User-Agent' => 'ruby'))
+      rescue OpenURI::HTTPError => e
+        puts "Can't access #{link}"
+        puts e.message
+        puts
+        next
+      end
+      new_article = Article.new
+      new_article.url_article = link
+      new_article.medium_id = @media.id
+      new_article.language = @media.language
+      new_article.category_article =  article.css('#content > nav > ol > li:nth-child(3) > a').text
+      new_article.title = article.css('article header.heading-a p',).text + ', '+ article.css('article header.heading-a h1',).text
+      # new_article.author = article.css('div.article-head__author div em a').text
+      author_exist = if article.css('h3.scheme-user').nil?
+                       Author.where(['lower(name) like ? ', ("L'expressiondz auteur").downcase])
+                     else
+                       a = article.at('h3.scheme-user').text
+                       Author.where(['lower(name) like ? ',
+                                     a.downcase])
+                     end
+
+      new_author = Author.new
+      if author_exist.count.zero?
+
+        new_author.name = article.css('h3.scheme-user').nil? ? "L'expressiondz auteur" : article.css('h3.scheme-user').text
+        new_author.medium_id = @media.id
+        new_author.save!
+        new_article.author_id = new_author.id
+      else
+        new_article.author_id = author_exist.first.id
+
+      end
+
+      new_article.body = article.css('div.module-article p').inner_html
+      new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
+
+      date_published_array = article.at('ul.list-details li').text
+      new_article.date_published = date_published_array.split('|')[1].to_datetime.change({ hour: 0, min: 0, sec: 0 })
+      url_array = article.css('figure.image-featured img').map{ |link|  link['data-src']}
+      new_article.url_image = url_array[0]
+      begin
+        if url_array[0].present?
+          new_article.image = Down.download(url_array[0])
+        end
+      rescue Down::Error => e
+        puts "Can't download this image #{url_array[0]}"
+        puts e.message
+        puts
+        new_article.image = nil
+      end
+      # tags_array = article.css('#tags a').map(&:text)
+      # new_article.media_tags = tags_array.join(',')
+      new_article.status = 'pending'
+      new_article.save!
+      #  tag_check_and_save(tags_array)if @media.tag_status == true
+    end
+    render json: { crawling_status_expression: 'ok' }
+  end
+  # end method to get expression articles
+  #
+
 
 
 
--- lib/tasks/crawling.rake
+++ lib/tasks/crawling.rake
@@ -2632,6 +2632,109 @@ article.css('div.post-header div.single-featured > a').map do |link|
 
 
 
+    # start method to get expression articles
+  def get_articles_lexpressiondz(url_media_array)
+    articles_url_lexpressiondz = []
+    url_media_array.map do |url|
+      begin
+        doc = Nokogiri::HTML(open(url, 'User-Agent' => 'ruby'))
+      rescue OpenURI::HTTPError => e
+        puts "Can't access #{url}"
+        puts e.message
+        puts
+        next
+      end
+
+      doc.css('article h2 a').map do |link|
+        articles_url_lexpressiondz << link['href']
+      end
+    end
+    articles_url_lexpressiondz = articles_url_lexpressiondz.reject(&:nil?)
+
+    articles_url_lexpressiondz_after_check = []
+    articles_url_lexpressiondz.map do |link|
+      articles_url_lexpressiondz_after_check << link unless Article.where(medium_id: @media.id,url_article: link).present?
+    end
+
+    articles_url_lexpressiondz_after_check.map do |link|
+
+
+      begin
+        article = Nokogiri::HTML(open(link, 'User-Agent' => 'ruby'))
+      rescue OpenURI::HTTPError => e
+        puts "Can't access #{link}"
+        puts e.message
+        puts
+        next
+      end
+      new_article = Article.new
+      new_article.url_article = link
+      new_article.medium_id = @media.id
+      new_article.language = @media.language
+      new_article.category_article =  article.css('#content > nav > ol > li:nth-child(3) > a').text
+      new_article.title = article.css('article header.heading-a p',).text + ', '+ article.css('article header.heading-a h1',).text
+      # new_article.author = article.css('div.article-head__author div em a').text
+      author_exist = if article.css('h3.scheme-user').nil?
+                       Author.where(['lower(name) like ? ', ("L'expressiondz auteur").downcase])
+                     else
+                       a = article.at('h3.scheme-user').text
+                       Author.where(['lower(name) like ? ',
+                                     a.downcase])
+                     end
+
+      new_author = Author.new
+      if author_exist.count.zero?
+
+        new_author.name = article.css('h3.scheme-user').nil? ? "L'expressiondz auteur" : article.css('h3.scheme-user').text
+        new_author.medium_id = @media.id
+        new_author.save!
+        new_article.author_id = new_author.id
+      else
+        new_article.author_id = author_exist.first.id
+
+      end
+
+      new_article.body = article.css('div.module-article p').inner_html
+      new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
+
+      date_published_array = article.at('ul.list-details li').text
+      new_article.date_published = date_published_array.split('|')[1].to_datetime.change({ hour: 0, min: 0, sec: 0 })
+      url_array = article.css('figure.image-featured img').map{ |link|  link['data-src']}
+      new_article.url_image = url_array[0]
+      begin
+        if url_array[0].present?
+          new_article.image = Down.download(url_array[0])
+        end
+      rescue Down::Error => e
+        puts "Can't download this image #{url_array[0]}"
+        puts e.message
+        puts
+        new_article.image = nil
+      end
+      # tags_array = article.css('#tags a').map(&:text)
+      # new_article.media_tags = tags_array.join(',')
+      new_article.status = 'pending'
+      new_article.save!
+      puts "URLBefoooooooooooooor:" + link
+      if Article.where(url_article: link).present?
+        puts 'article present'
+      else
+        articlesTagsUrl = link
+      end
+      puts "URLURLURLURLURLURLURLURLURLURLURLURLURLURLURL: #{articlesTagsUrl}"
+
+      new_article.save!
+      if articlesTagsUrl.present?
+        puts 'add article'
+        @articles_for_auto_tag << Article.where(url_article: articlesTagsUrl)[0]
+      end
+
+    end
+    puts "json: { crawling_status_expression: 'ok' }"
+  end
+    # end method to get expression articles
+    #
+
 
 
 
