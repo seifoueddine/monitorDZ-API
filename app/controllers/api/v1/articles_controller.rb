97dfa087c11196c491e25a3ca53b9c4fe5d65b39
Seif Eddine .N <nouaraseifeddine@gmail.com>
2021-12-22 21:04:08 +0100

    change sttings

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,6 +2,7 @@
 
 module Api
   module V1
+    # crawling articles
     class ArticlesController < ::ApplicationController
       # before_action :authenticate_user! , except: :pdf_export
       before_action :set_article, only: %i[show update destroy]
