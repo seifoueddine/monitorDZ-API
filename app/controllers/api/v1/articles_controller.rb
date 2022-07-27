5d5cfc77b62f1ac10fb44487a74b3a0340b2814a
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-07-27 21:07:40 +0200

    add ennahar file

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative '../../../../lib/articles/export'
+require_relative '../../../../lib/articles/crawling/ennahar'
 module Api
   module V1
     # crawling articles
@@ -341,139 +342,6 @@ module Api
         pdf = WickedPdf.new.pdf_from_string(@html)
         send_data pdf, filename: "Article_#{@article.id}.pdf", type: 'application/pdf'
       end
-
-      # def get_html_fr
-      #   '<!DOCTYPE html>
-      #   <html>
-      #     <head>
-      #       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      #   <style>
-      #         div.alwaysbreak { page-break-before: always; }
-      #   div.nobreak:before { clear:both; }
-      #   div.nobreak { page-break-inside: avoid; }
-      #       </style>
-      #     </head>
-      #     <body>
-
-      #   <div leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0"
-      #         style="height:auto !important;width:100% !important; margin-bottom: 40px;">
-      #         <div class="justify-content-center d-flex">
-      #           <table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0"
-      #             style="max-width:600px; background-color:#ffffff;border:1px solid #e4e2e2;border-collapse:separate !important; border-radius:10px;border-spacing:0;color:#242128; margin:0;padding:40px;"
-      #             heigth="auto">
-      #             <tbody>
-      #               <tr>
-      #                 <td align="left" valign="center"
-      #                 style="padding-bottom:40px;border-top:0;height:100% !important;width:150px !important;">
-      #                   <img style="height:100px" src="' + 'https://api.mediasmonitoring.com' + @article.medium.avatar.url + ' " />
-      #                 </td>
-      #                 <td align="center" valign="center"
-      #                   style="padding-bottom:40px;border-top:0;height:100% !important;width:auto !important;">
-      #                   <span style="color: #8f8f8f; font-weight: normal; line-height: 2; font-size: 14px;"> ' + @article.author.name + ' | ' + @article.date_published.strftime('%d - %m - %Y') + '</span>
-      #                 </td>
-      #   <td align="center" valign="center"
-      #                   style="padding-bottom:40px;border-top:0;height:100% !important;width:auto !important;">
-
-      #                 </td>
-      #               </tr>
-      #               <tr>
-      #                 <td colSpan="2" style="padding-top:10px;border-top:1px solid #e4e2e2">
-      #                   <h2 style="color:#303030; font-size:20px; line-height: 1.6; font-weight:500;"><b>' + @article.title + '</b> </h2>
-      #                   ' + @article.body + '
-      #                 </td>
-      #               </tr>
-      #             </tbody>
-      #           </table>
-      #         </div>
-      #         <div class="justify-content-center d-flex">
-      #           <table style="margin-top:30px; padding-bottom:20px;; margin-bottom: 40px;">
-      #             <tbody>
-      #               <tr>
-      #                 <td align="center" valign="center">
-      #                   <p
-      #                     style="font-size: 12px;line-height: 1; color:#909090; margin-top:0px; margin-bottom:5px; ">
-      #                     PDF généré par MediaDZ app le ' + Date.today.strftime('%d - %m - %Y') + '
-      #                   </p>
-      #                   <p style="font-size: 12px; line-height:1; color:#909090;  margin-top:5px; margin-bottom:5px;">
-      #                     <a href="#" style="color: #00365a;text-decoration:none;">Alger</a> , <a href="#"
-      #                       style="color: #00365a;text-decoration:none; ">Algerie</a>
-      #                   </p>
-      #                 </td>
-      #               </tr>
-      #             </tbody>
-      #           </table>
-      #         </div>
-      #       </div>
-      #     </body>
-      #   </html>'
-      # end
-
-      # def get_html_ar
-      #   '<!DOCTYPE html>
-      #   <html>
-      #     <head>
-      #       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      #   <style>
-      #         div.alwaysbreak { page-break-before: always; }
-      #   div.nobreak:before { clear:both; }
-      #   div.nobreak { page-break-inside: avoid; }
-      #       </style>
-      #     </head>
-      #     <body>
-
-      #   <div leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0"
-      #         style="height:auto !important;width:100% !important; margin-bottom: 40px;">
-      #         <div class="justify-content-center d-flex">
-      #           <table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0"
-      #             style="max-width:600px; background-color:#ffffff;border:1px solid #e4e2e2;border-collapse:separate !important; border-radius:10px;border-spacing:0;color:#242128; margin:0;padding:40px;"
-      #             heigth="auto">
-      #             <tbody>
-      #               <tr>
-      #                 <td align="left" valign="center"
-      #                 style="padding-bottom:40px;border-top:0;height:100% !important;width:30% !important;">
-      #                   <img style="height:100px" src="' + 'https://api.mediasmonitoring.com' + @article.medium.avatar.url + ' " />
-      #                 </td>
-      #                 <td align="center" valign="center"
-      #                   style="padding-bottom:40px;border-top:0;height:100% !important;width:auto  !important;">
-      #                   <span style="color: #8f8f8f; font-weight: normal; line-height: 2; font-size: 14px;">' + @article.author.name + ' | ' + @article.date_published.strftime('%d - %m - %Y') + '</span>
-      #                 </td>
-      #               <td align="center" valign="center"
-      #                   style="padding-bottom:40px;border-top:0;height:100% !important;width:auto !important;">
-
-      #                 </td>
-      #               </tr>
-      #               <tr>
-      #                 <td colSpan="2" style="padding-top:10px;border-top:1px solid #e4e2e2;direction: rtl;">
-      #                   <h2 style="color:#303030; font-size:20px; line-height: 1.6; font-weight:500;direction: rtl;"><b>' + @article.title + ' </b></h2>
-      #                   ' + @article.body + '
-      #                 </td>
-      #               </tr>
-      #             </tbody>
-      #           </table>
-      #         </div>
-      #         <div class="justify-content-center d-flex">
-      #           <table style="margin-top:30px; padding-bottom:20px;; margin-bottom: 40px;">
-      #             <tbody>
-      #               <tr>
-      #                 <td align="center" valign="center">
-      #                   <p
-      #                     style="font-size: 12px;line-height: 1; color:#909090; margin-top:0px; margin-bottom:5px; ">
-      #                     PDF généré par MediaDZ app le ' + Date.today.strftime('%d - %m - %Y') + '
-      #                   </p>
-      #                   <p style="font-size: 12px; line-height:1; color:#909090;  margin-top:5px; margin-bottom:5px;">
-      #                     <a href="#" style="color: #00365a;text-decoration:none;">Alger</a> , <a href="#"
-      #                       style="color: #00365a;text-decoration:none; ">Algerie</a>
-      #                   </p>
-      #                 </td>
-      #               </tr>
-      #             </tbody>
-      #           </table>
-      #         </div>
-      #       </div>
-      #     </body>
-      #   </html>'
-      # end
-
       # export PDF
 
       def send_email
@@ -648,7 +516,7 @@ module Api
         when 'ELCHEROUK'
           get_articles_elcherouk(url_media_array)
         when 'ENNAHAR'
-          get_articles_ennahar(url_media_array)
+          Articles::Crawling::Ennahar.get_articles_ennahar(url_media_array)
         when 'TSA'
           get_articles_tsa(url_media_array)
         when 'APS'
@@ -916,91 +784,91 @@ module Api
       # end method to get elcherouk articles
 
       # start method to get ennahar articles
-      def get_articles_ennahar(url_media_array)
-        articles_url_ennahar = []
-        last_dates = []
-        url_media_array.map do |url|
-          # doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'ruby/2.6.5'))
+      # def get_articles_ennahar(url_media_array)
+      #   articles_url_ennahar = []
+      #   last_dates = []
+      #   url_media_array.map do |url|
+      #     # doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'ruby/2.6.5'))
 
-          begin
-            doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'ruby/2.6.5'))
-          rescue OpenURI::HTTPError => e
-            puts "Can't access #{url}"
-            puts e.message
-            puts
-            next
-          end
+      #     begin
+      #       doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'ruby/2.6.5'))
+      #     rescue OpenURI::HTTPError => e
+      #       puts "Can't access #{url}"
+      #       puts e.message
+      #       puts
+      #       next
+      #     end
 
-          doc.css('h2.card__title.x-middle a').map do |link|
-            articles_url_ennahar << link['href']
-          end
-          doc.css('time').map do |date|
-            last_dates << date['datetime']
-          end
-        end
-        last_dates = last_dates.map { |d| d.to_datetime.change({ hour: 0, min: 0, sec: 0 }) + (1.0 / 24) }
-        articles_url_ennahar = articles_url_ennahar.reject(&:nil?)
-        last_dates = last_dates.uniq
-        last_articles = Article.where(medium_id: @media.id).where(date_published: last_dates)
-        list_articles_url = []
-        last_articles.map do |article|
-          list_articles_url << article.url_article
-        end
-        articles_url_ennahar_after_check = articles_url_ennahar - list_articles_url
-        articles_url_ennahar_after_check.map do |link|
-          begin
-            article = Nokogiri::HTML(URI.open(link, 'User-Agent' => 'ruby/2.6.5'))
-          rescue OpenURI::HTTPError => e
-            puts "Can't access #{link}"
-            puts e.message
-            puts
-            next
-          end
+      #     doc.css('h2.card__title.x-middle a').map do |link|
+      #       articles_url_ennahar << link['href']
+      #     end
+      #     doc.css('time').map do |date|
+      #       last_dates << date['datetime']
+      #     end
+      #   end
+      #   last_dates = last_dates.map { |d| d.to_datetime.change({ hour: 0, min: 0, sec: 0 }) + (1.0 / 24) }
+      #   articles_url_ennahar = articles_url_ennahar.reject(&:nil?)
+      #   last_dates = last_dates.uniq
+      #   last_articles = Article.where(medium_id: @media.id).where(date_published: last_dates)
+      #   list_articles_url = []
+      #   last_articles.map do |article|
+      #     list_articles_url << article.url_article
+      #   end
+      #   articles_url_ennahar_after_check = articles_url_ennahar - list_articles_url
+      #   articles_url_ennahar_after_check.map do |link|
+      #     begin
+      #       article = Nokogiri::HTML(URI.open(link, 'User-Agent' => 'ruby/2.6.5'))
+      #     rescue OpenURI::HTTPError => e
+      #       puts "Can't access #{link}"
+      #       puts e.message
+      #       puts
+      #       next
+      #     end
 
-          new_article = Article.new
-          new_article.url_article = link
-          new_article.medium_id = @media.id
-          new_article.language = @media.language
-          new_article.category_article = article.css('div.sgb1__acat a').text
-          new_article.title = article.css('h1.sgb1__attl').text
-          # new_article.author = article.css('div.article-head__author div em a').text
+      #     new_article = Article.new
+      #     new_article.url_article = link
+      #     new_article.medium_id = @media.id
+      #     new_article.language = @media.language
+      #     new_article.category_article = article.css('div.sgb1__acat a').text
+      #     new_article.title = article.css('h1.sgb1__attl').text
+      #     # new_article.author = article.css('div.article-head__author div em a').text
 
-          author_exist = Author.where(['lower(name) like ? ',
-                                       article.at('div.sgb1__aath a').text.downcase])
+      #     author_exist = Author.where(['lower(name) like ? ',
+      #                                  article.at('div.sgb1__aath a').text.downcase])
 
-          new_author = Author.new
-          if author_exist.count.zero?
+      #     new_author = Author.new
+      #     if author_exist.count.zero?
 
-            new_author.name = article.at('div.sgb1__aath a').text
-            new_author.medium_id = @media.id
-            new_author.save!
-            new_article.author_id = new_author.id
-          else
-            new_article.author_id = author_exist.first.id
+      #       new_author.name = article.at('div.sgb1__aath a').text
+      #       new_author.medium_id = @media.id
+      #       new_author.save!
+      #       new_article.author_id = new_author.id
+      #     else
+      #       new_article.author_id = author_exist.first.id
 
-          end
-          new_article.body = article.css('div.artx').inner_html
-          new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
-          new_article.date_published = article.at('time[datetime]')['datetime'].to_datetime.change({ hour: 0, min: 0,
-                                                                                                     sec: 0 }) + (1.0 / 24)
-          url_array = article.css('figure.sgb1__afig div.sgb1__afmg img').map { |link| link['data-src'] }
-          new_article.url_image = url_array[0]
-          begin
-            new_article.image = Down.download(url_array[0]) if url_array[0].present?
-          rescue Down::Error => e
-            puts "Can't download this image #{url_array[0]}"
-            puts e.message
-            puts
-            new_article.image = nil
-          end
-          # tags_array = article.css('div.article-core__tags a').map(&:text)
-          # new_article.media_tags = tags_array.join(',')
-          new_article.status = 'pending'
-          new_article.save!
-          # tag_check_and_save(tags_array)
-        end
-        render json: { crawling_status_ennahar: 'ok' }
-      end
+      #     end
+      #     new_article.body = article.css('div.artx').inner_html
+      #     new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
+      #     new_article.date_published = article.at('time[datetime]')['datetime'].to_datetime.change({ hour: 0, min: 0,
+      #                                                                                                sec: 0 }) + (1.0 / 24)
+      #     url_array = article.css('figure.sgb1__afig div.sgb1__afmg img').map { |link| link['data-src'] }
+      #     new_article.url_image = url_array[0]
+      #     begin
+      #       new_article.image = Down.download(url_array[0]) if url_array[0].present?
+      #     rescue Down::Error => e
+      #       puts "Can't download this image #{url_array[0]}"
+      #       puts e.message
+      #       puts
+      #       new_article.image = nil
+      #     end
+      #     # tags_array = article.css('div.article-core__tags a').map(&:text)
+      #     # new_article.media_tags = tags_array.join(',')
+      #     new_article.status = 'pending'
+      #     new_article.save!
+      #     # tag_check_and_save(tags_array)
+      #   end
+      #   render json: { crawling_status_ennahar: 'ok' }
+      # end
       # end method to get ennahar articles
 
       # start method to get TSA articles
@@ -4262,7 +4130,7 @@ module Api
                       :media_tags, :language, :url_image, :author_id)
       end
 
-      # tag_check_and_savetag_check_and_save
+      # tag_check_and_savetag_check_and_save not used yet 
       def tag_check_and_save(tags_array)
         tags_array.map do |t|
           # tag_exist = Tag.where(['lower(name) like ? ',
---
+++ lib/articles/crawling/ennahar.rb
@@ -0,0 +1,91 @@
+module Articles
+    class Ennahar
+        class << self 
+            def get_articles_ennahar(url_media_array)
+                articles_url_ennahar = []
+                last_dates = []
+                url_media_array.map do |url|
+                  
+        
+                  begin
+                    doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'ruby/2.6.5'))
+                  rescue OpenURI::HTTPError => e
+                    puts "Can't access #{url}"
+                    puts e.message
+                    puts
+                    next
+                  end
+        
+                  doc.css('h2.card__title.x-middle a').map do |link|
+                    articles_url_ennahar << link['href']
+                  end
+                  doc.css('time').map do |date|
+                    last_dates << date['datetime']
+                  end
+                end
+                last_dates = last_dates.map { |d| d.to_datetime.change({ hour: 0, min: 0, sec: 0 }) + (1.0 / 24) }
+                articles_url_ennahar = articles_url_ennahar.reject(&:nil?)
+                last_dates = last_dates.uniq
+                last_articles = Article.where(medium_id: @media.id).where(date_published: last_dates)
+                list_articles_url = []
+                last_articles.map do |article|
+                  list_articles_url << article.url_article
+                end
+                articles_url_ennahar_after_check = articles_url_ennahar - list_articles_url
+                articles_url_ennahar_after_check.map do |link|
+                  begin
+                    article = Nokogiri::HTML(URI.open(link, 'User-Agent' => 'ruby/2.6.5'))
+                  rescue OpenURI::HTTPError => e
+                    puts "Can't access #{link}"
+                    puts e.message
+                    puts
+                    next
+                  end
+        
+                  new_article = Article.new
+                  new_article.url_article = link
+                  new_article.medium_id = @media.id
+                  new_article.language = @media.language
+                  new_article.category_article = article.css('div.sgb1__acat a').text
+                  new_article.title = article.css('h1.sgb1__attl').text
+                  # new_article.author = article.css('div.article-head__author div em a').text
+        
+                  author_exist = Author.where(['lower(name) like ? ',
+                                               article.at('div.sgb1__aath a').text.downcase])
+        
+                  new_author = Author.new
+                  if author_exist.count.zero?
+        
+                    new_author.name = article.at('div.sgb1__aath a').text
+                    new_author.medium_id = @media.id
+                    new_author.save!
+                    new_article.author_id = new_author.id
+                  else
+                    new_article.author_id = author_exist.first.id
+        
+                  end
+                  new_article.body = article.css('div.artx').inner_html
+                  new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
+                  new_article.date_published = article.at('time[datetime]')['datetime'].to_datetime.change({ hour: 0, min: 0,
+                                                                                                             sec: 0 }) + (1.0 / 24)
+                  url_array = article.css('figure.sgb1__afig div.sgb1__afmg img').map { |link| link['data-src'] }
+                  new_article.url_image = url_array[0]
+                  begin
+                    new_article.image = Down.download(url_array[0]) if url_array[0].present?
+                  rescue Down::Error => e
+                    puts "Can't download this image #{url_array[0]}"
+                    puts e.message
+                    puts
+                    new_article.image = nil
+                  end
+                  # tags_array = article.css('div.article-core__tags a').map(&:text)
+                  # new_article.media_tags = tags_array.join(',')
+                  new_article.status = 'pending'
+                  new_article.save!
+                  # tag_check_and_save(tags_array)
+                end
+                render json: { crawling_status_ennahar: 'ok' }
+              end
+        end    
+    end
+end
