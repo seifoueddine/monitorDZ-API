25dc7295c8e0d10dcdb036218b5aedb56eb89f17
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-07-27 14:10:37 +0200

    delete all dependent

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -19,8 +19,8 @@ class Campaign < ApplicationRecord
   belongs_to :slug
   has_many :campaign_sectors
   has_many :sectors, through: :campaign_sectors
-  has_many :campaign_media
+  has_many :campaign_media , dependent: :delete_all
   has_many :media, through: :campaign_media
-  has_many :campaign_tags
+  has_many :campaign_tags , dependent: :delete_all
   has_many :tags, through: :campaign_tags
 end
