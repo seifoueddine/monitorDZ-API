47e70cbe5cfd804c5354ad08aa173066e3921d91
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-07-31 02:32:12 +0200

    add elkhabar file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,4 +37,135 @@ class ApplicationController < ActionController::API
     base_uri = request.url.split('?').first
     "<#{base_uri}?#{request.query_parameters.merge(page: page).to_param}>; rel='#{rel}'"
   end
+
+  def change_translate_date(d)
+    d.split.map do |m|
+      case m.downcase
+      when 'Janvier,'.downcase
+        'January'
+      when 'Février,'.downcase
+        'February'
+      when 'Mars,'.downcase
+        'March'
+      when 'Avril,'.downcase
+        'April'
+      when 'Mai,'.downcase
+        'May'
+      when 'Juin,'.downcase
+        'June'
+      when 'Juillet,'.downcase
+        'July'
+      when 'Octobre,'.downcase
+        'October'
+      when 'Novembre,'.downcase
+        'November'
+      when 'Décembre,'.downcase
+        'December'
+      when 'Septembre,'.downcase
+        'September'
+      when 'août,'.downcase
+        'August'
+      when 'Janvier'.downcase
+        'January'
+      when 'Février'.downcase
+        'February'
+      when 'Mars'.downcase
+        'March'
+      when 'Avril'.downcase
+        'April'
+      when 'Mai'.downcase
+        'May'
+      when 'Juin'.downcase
+        'June'
+      when 'Juillet'.downcase
+        'July'
+      when 'Octobre'.downcase
+        'October'
+      when 'Novembre'.downcase
+        'November'
+      when 'Décembre'.downcase
+        'December'
+      when 'Septembre'.downcase
+        'September'
+      when 'août'.downcase
+        'August'
+
+      when 'جانفي'.downcase
+        'January'
+      when 'فيفري'.downcase
+        'February'
+      when 'مارس'.downcase
+        'March'
+      when 'افريل'.downcase
+        'April'
+      when 'ماي'.downcase
+        'May'
+      when 'جوان'.downcase
+        'June'
+      when 'جويلية'.downcase
+        'July'
+      when 'جولية'.downcase
+        'July'
+      when 'أكتوبر'.downcase
+        'October'
+      when 'نوفمبر'.downcase
+        'November'
+      when 'ديسمبر'.downcase
+        'December'
+      when 'سبتمبر'.downcase
+        'September'
+      when 'اوت'.downcase
+        'August'
+
+      when 'جانفي،'.downcase
+        'January'
+      when 'فيفري،'.downcase
+        'February'
+      when 'مارس،'.downcase
+        'March'
+      when 'افريل،'.downcase
+        'April'
+      when 'ماي،'.downcase
+        'May'
+      when 'جوان،'.downcase
+        'June'
+      when 'جويلية،'.downcase
+        'July'
+      when 'جولية،'.downcase
+        'July'
+      when 'أكتوبر،'.downcase
+        'October'
+      when 'نوفمبر،'.downcase
+        'November'
+      when 'نونمبر،'.downcase
+        'November'
+      when 'ديسمبر،'.downcase
+        'December'
+      when 'سبتمبر،'.downcase
+        'September'
+      when 'اوت،'.downcase
+        'August'
+
+      when 'يناير'.downcase
+        'January'
+      when 'فبراير'.downcase
+        'February'
+      when 'ابريل'.downcase
+        'April'
+      when 'أبريل'.downcase
+        'April'
+      when 'مايو'.downcase
+        'May'
+      when 'يونيو'.downcase
+        'June'
+      when 'يوليو'.downcase
+        'July'
+      when 'أغسطس'.downcase
+        'August'
+
+      else
+        m
+      end
+    end.join(' ')
+  end
 end
