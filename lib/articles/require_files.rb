03a447167e5096c03c2a8362f29064a44c44db6a
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-08-06 21:36:01 +0200

    refacto

--- lib/articles/require_files.rb
+++ lib/articles/require_files.rb
@@ -1,3 +1,5 @@
+module Articles
+    class RequireFiles
 require_relative '../../../../lib/articles/crawling/ennahar'
 require_relative '../../../../lib/articles/crawling/elcherouk'
 require_relative '../../../../lib/articles/crawling/tsa'
@@ -8,3 +10,5 @@ require_relative '../../../../lib/articles/crawling/lexpressiondz'
 require_relative '../../../../lib/articles/crawling/algerie360'
 require_relative '../../../../lib/articles/crawling/visaalgerie'
 require_relative '../../../../lib/articles/crawling/alyaoum24'
+    end
+end
