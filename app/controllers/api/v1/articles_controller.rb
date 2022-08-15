9bbd2efed101c2b6604160adb2af41d13619debc
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-08-15 18:01:23 +0200

    add files 24hdz

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -14,7 +14,7 @@ require_relative '../../../../lib/articles/crawling/alyaoum24'
 require_relative '../../../../lib/articles/crawling/maroco360'
 require_relative '../../../../lib/articles/crawling/radioalgerie_ar'
 require_relative '../../../../lib/articles/crawling/radioalgerie_fr'
-
+require_relative '../../../../lib/articles/crawling/hdz24'
 
 module Api
   module V1
@@ -573,7 +573,7 @@ module Api
         when 'ALGERIEPARTPLUS'
           get_articles_algerie_part(url_media_array)
         when '24H-DZ'
-          get_articles_24hdz(url_media_array)
+          articles_24hdz_crawler(url_media_array, @media)
         when 'REPORTERS'
           articles_reporters_crawler(url_media_array, @media)
         when 'SHIHABPRESSE'
@@ -1180,97 +1180,9 @@ module Api
       # end method to get _liberte_ar articles
 
       # start method to get 24hdz articles
-      def get_articles_24hdz(url_media_array)
-        articles_url_24hdz = []
-        url_media_array.map do |url|
-          puts "Start category parsing : #{url} :) "
-          begin
-            doc = Nokogiri::HTML(URI.open(url))
-          rescue OpenURI::HTTPError => e
-            puts "Can't access #{url}"
-            puts e.message
-            puts
-            next
-          end
-          doc.css('h3.entry-title.td-module-title a').map do |link|
-            articles_url_24hdz << link['href']
-          end
-        end
-
-        articles_url_24hdz = articles_url_24hdz.reject(&:nil?)
-
-        articles_url_24hdz_after_check = []
-        articles_url_24hdz.map do |link|
-          articles_url_24hdz_after_check << link unless Article.where(medium_id: @media.id, url_article: link).present?
-        end
-
-        articles_url_24hdz_after_check.map do |link|
-          puts "Start article parsing : #{link} :) "
-          begin
-            article = Nokogiri::HTML(URI.open(link))
-          rescue OpenURI::HTTPError => e
-            puts "Can't access #{link}"
-            puts e.message
-            puts
-            next
-          end
-          new_article = Article.new
-          new_article.url_article = link
-          new_article.medium_id = @media.id
-          new_article.language = @media.language
-          new_article.category_article = article.css('span a.entry-crumb').text
-          new_article.title = article.css('header.td-post-title').text
-          #  new_article.author = article.css('div.article-head__author div em a').text
-          author_exist = if article.at('div.td-post-author-name').nil?
-                           Author.where(['lower(name) like ? ', '24h-dz auteur'.downcase])
-                         else
-                           Author.where(['lower(name) like ? ',
-                                         article.at('div.td-post-author-name').text.downcase])
-                         end
-
-          new_author = Author.new
-          if author_exist.count.zero?
-
-            new_author.name = article.at('div.td-post-author-name').nil? ? '24h-dz auteur' : article.at('div.td-post-author-name').text
-            new_author.medium_id = @media.id
-            new_author.save!
-            new_article.author_id = new_author.id
-          else
-            new_article.author_id = author_exist.first.id
-
-          end
-
-          new_article.body = article.css('div.td-post-content').inner_html
-          new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
-
-          # d = change_date_autobip_aps(date)
-          new_article.date_published = article.at('time[datetime]')['datetime'].to_datetime.change({ hour: 0, min: 0,
-                                                                                                     sec: 0 }) + (1.0 / 24)
-          # new_article.date_published =
-          url_array = article.css('div.td-post-featured-image img').map { |link| link['src'] }
-          new_article.url_image = url_array[0]
-          begin
-            new_article.image = Down.download(url_array[0]) if url_array[0].present?
-          rescue Down::Error => e
-            puts "Can't download this image #{url_array[0]}"
-            puts e.message
-            puts
-            new_article.image = nil
-          end
-          # tags_array = article.css('ul.itemTags li').map(&:text)
-          # new_article.media_tags = tags_array.join(',')
-          new_article.status = 'pending'
-          puts "URLBefoooooooooooooor:#{link}"
-          if Article.where(url_article: link).present?
-            puts 'article present'
-          else
-            articlesTagsUrl = link
-          end
-          puts "URLURLURLURLURLURLURLURLURLURLURLURLURLURLURL: #{articlesTagsUrl}"
-
-          new_article.save!
-        end
-        render json: { crawling_status_24hdz: 'ok' }
+      def articles_24hdz_crawler(url_media_array, media)
+        count = Articles::Crawling::Hdz24.get_articles_24hdz(url_media_array, media)
+        render json: { crawling_24hdz: count }
       end
       # end method to get 24hdz articles
 
