db4492d1fa0c89cb3159b80b151dd4977286b0e8
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-08-15 19:07:11 +0200

    add maghrebemergent file

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -15,7 +15,7 @@ require_relative '../../../../lib/articles/crawling/maroco360'
 require_relative '../../../../lib/articles/crawling/radioalgerie_ar'
 require_relative '../../../../lib/articles/crawling/radioalgerie_fr'
 require_relative '../../../../lib/articles/crawling/hdz24'
-
+require_relative '../../../../lib/articles/crawling/maghrebemergent'
 module Api
   module V1
     # crawling articles
@@ -539,7 +539,7 @@ module Api
         when 'APS-AR'
           get_articles_aps_ar(url_media_array)
         when 'MAGHREBEMERGENT'
-          get_articles_maghrebemergent(url_media_array)
+          articles_maghrebemergent_crawler(url_media_array, @media)
         when 'ELBILAD'
           articles_bilad_crawler(url_media_array, @media)
         when 'ELMOUDJAHID'
@@ -1202,99 +1202,12 @@ module Api
       #
 
       # start method to get maghrebemergent articles
-      def get_articles_maghrebemergent(url_media_array)
-        articles_url_maghrebemergent = []
-        last_dates = []
-        url_media_array.map do |url|
-          begin
-            doc = Nokogiri::HTML(URI.open(url))
-          rescue OpenURI::HTTPError => e
-            puts "Can't access #{url}"
-            puts e.message
-            puts
-            next
-          end
-          doc.css('article a.elementor-post__thumbnail__link').map do |link|
-            articles_url_maghrebemergent << link['href']
-          end
-          doc.css('article div div span.elementor-post-date').map do |date|
-            last_dates << date.text
-          end
-        end
-        last_dates = last_dates.map { |d| change_translate_date(d) }
-        last_dates = last_dates.map { |d| d.to_datetime.change({ hour: 0, min: 0, sec: 0 }) }
-        articles_url_maghrebemergent = articles_url_maghrebemergent.reject(&:nil?)
-        last_dates = last_dates.uniq
-        last_articles = Article.where(medium_id: @media.id).where(date_published: last_dates)
-        list_articles_url = []
-        last_articles.map do |article|
-          list_articles_url << article.url_article
-        end
-        articles_url_maghrebemergent_after_check = articles_url_maghrebemergent - list_articles_url
-        articles_url_maghrebemergent_after_check.map do |link|
-          begin
-            article = Nokogiri::HTML(URI.open(link))
-          rescue OpenURI::HTTPError => e
-            puts "Can't access #{link}"
-            puts e.message
-            puts
-            next
-          end
-          new_article = Article.new
-          new_article.url_article = link
-          new_article.medium_id = @media.id
-          new_article.language = @media.language
-          new_article.category_article = article.at('div.elementor-widget-container ul li span span.elementor-post-info__terms-list a').text
-          new_article.title = article.css('h1.elementor-heading-title.elementor-size-small').text
-          # new_article.author = article.css('div.article-head__author div em a').text
-
-          if article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').nil?
-            author_exist = Author.where(['lower(name) like ? ', 'Maghrebemergent auteur'.downcase])
-          else
-            author_exist = Author.where(['lower(name) like ? ',
-                                         article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').text.downcase])
-          end
-
-          new_author = Author.new
-          if author_exist.count.zero?
-
-            new_author.name = article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').nil? ? 'Maghrebemergent auteur' : article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').text
-            new_author.medium_id = @media.id
-            new_author.save!
-            new_article.author_id = new_author.id
-          else
-            new_article.author_id = author_exist.first.id
-
-          end
-          new_article.body = article.css('div.elementor-element.elementor-element-c93088c.elementor-widget.elementor-widget-theme-post-content').inner_html
-          new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
-          # date = article.at('p.text-capitalize span').text
-          # date[','] = ''
-          date = article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text.elementor-post-info__item.elementor-post-info__item--type-date').text
-          d = change_translate_date(date)
-          new_article.date_published = d.to_datetime.change({ hour: 0, min: 0, sec: 0 })
-          url_array = article.css('section div div div div div div.elementor-widget-wrap div.elementor-widget-container div.elementor-image img').map do |link|
-            link['src']
-          end
-          new_article.url_image = url_array[1]
-          begin
-            new_article.image = Down.download(url_array[0]) if url_array[0].present?
-          rescue Down::Error => e
-            puts "Can't download this image #{url_array[0]}"
-            puts e.message
-            puts
-            new_article.image = nil
-          end
-          # tags_array = article.css('ul.itemTags li').map(&:text)
-          # new_article.media_tags = tags_array.join(',')
-          new_article.status = 'pending'
-          new_article.save!
-          # tag_check_and_save(tags_array)
-        end
-        render json: { crawling_status_aps: 'ok' }
+      def articles_maghrebemergent_crawler(url_media_array, media)
+        count = Articles::Crawling::Maghrebemergent.get_articles_maghrebemergent(url_media_array, media)
+        render json: { crawling_maghrebemergent: count }
       end
       # end method to get maghrebemergent articles
-      #
+      
 
       # start method to get elmoudjahid articles
       def get_articles_elmoudjahid_fr(url_media_array)
@@ -1370,9 +1283,7 @@ module Api
       end
       # end method to get elmoudjahid articles
       # start method to get elmoudjahid_fr articles
-      #
-      #
-      #
+      
 
       def get_articles_elmoudjahid(url_media_array)
         articles_url_elmoudjahid = []
@@ -1672,7 +1583,7 @@ module Api
         end
         render json: { crawling_status_aps: 'ok' }
       end
-      # end method to get elikhbaria articles
+      # end method to get elikhba ria articles
 
       # start method to get algerieco articles
       def get_articles_algerieco(url_media_array)
---
+++ lib/articles/crawling/maghrebemergent.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+module Articles
+    # crawling files
+    module Crawling
+      # methode to get Maghrebemergent articles
+      class Maghrebemergent
+        class << self
+          include AbstractController::Rendering 
+
+          def get_articles_maghrebemergent(url_media_array)
+            articles_url_maghrebemergent = []
+            count = 0
+            last_dates = []
+            url_media_array.map do |url|
+              begin
+                doc = Nokogiri::HTML(URI.open(url))
+              rescue OpenURI::HTTPError => e
+                puts "Can't access #{url}"
+                puts e.message
+                puts
+                next
+              end
+              doc.css('article a.elementor-post__thumbnail__link').map do |link|
+                articles_url_maghrebemergent << link['href']
+              end
+              doc.css('article div div span.elementor-post-date').map do |date|
+                last_dates << date.text
+              end
+            end
+            last_dates = last_dates.map { |d| change_translate_date(d) }
+            last_dates = last_dates.map { |d| d.to_datetime.change({ hour: 0, min: 0, sec: 0 }) }
+            articles_url_maghrebemergent = articles_url_maghrebemergent.reject(&:nil?)
+            last_dates = last_dates.uniq
+            last_articles = Article.where(medium_id: @media.id).where(date_published: last_dates)
+            list_articles_url = []
+            last_articles.map do |article|
+              list_articles_url << article.url_article
+            end
+            articles_url_maghrebemergent_after_check = articles_url_maghrebemergent - list_articles_url
+            articles_url_maghrebemergent_after_check.map do |link|
+              begin
+                article = Nokogiri::HTML(URI.open(link))
+              rescue OpenURI::HTTPError => e
+                puts "Can't access #{link}"
+                puts e.message
+                puts
+                next
+              end
+              new_article = Article.new
+              new_article.url_article = link
+              new_article.medium_id = @media.id
+              new_article.language = @media.language
+              new_article.category_article = article.at('div.elementor-widget-container ul li span span.elementor-post-info__terms-list a').text
+              new_article.title = article.css('h1.elementor-heading-title.elementor-size-small').text
+              # new_article.author = article.css('div.article-head__author div em a').text
+    
+              if article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').nil?
+                author_exist = Author.where(['lower(name) like ? ', 'Maghrebemergent auteur'.downcase])
+              else
+                author_exist = Author.where(['lower(name) like ? ',
+                                             article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').text.downcase])
+              end
+    
+              new_author = Author.new
+              if author_exist.count.zero?
+    
+                new_author.name = article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').nil? ? 'Maghrebemergent auteur' : article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text elementor-post-info__item elementor-post-info__item--type-author').text
+                new_author.medium_id = @media.id
+                new_author.save!
+                new_article.author_id = new_author.id
+              else
+                new_article.author_id = author_exist.first.id
+    
+              end
+              new_article.body = article.css('div.elementor-element.elementor-element-c93088c.elementor-widget.elementor-widget-theme-post-content').inner_html
+              new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
+              # date = article.at('p.text-capitalize span').text
+              # date[','] = ''
+              date = article.at('div.elementor-widget-container ul li a span.elementor-icon-list-text.elementor-post-info__item.elementor-post-info__item--type-date').text
+              d = change_translate_date(date)
+              new_article.date_published = d.to_datetime.change({ hour: 0, min: 0, sec: 0 })
+              url_array = article.css('section div div div div div div.elementor-widget-wrap div.elementor-widget-container div.elementor-image img').map do |link|
+                link['src']
+              end
+              new_article.url_image = url_array[1]
+              begin
+                new_article.image = Down.download(url_array[0]) if url_array[0].present?
+              rescue Down::Error => e
+                puts "Can't download this image #{url_array[0]}"
+                puts e.message
+                puts
+                new_article.image = nil
+              end
+              # tags_array = article.css('ul.itemTags li').map(&:text)
+              # new_article.media_tags = tags_array.join(',')
+              new_article.status = 'pending'
+              new_article.save!
+              count += 1 if new_article.save
+            end
+            count
+          end
+        end
+      end
+    end
+end
