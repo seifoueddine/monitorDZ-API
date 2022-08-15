ddc5d4a5b05094753e43c710e57e524ee9e988af
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-08-15 23:02:47 +0200

    add global methodes

--- lib/articles/crawling/maghrebemergent.rb
+++ lib/articles/crawling/maghrebemergent.rb
@@ -7,7 +7,7 @@ module Articles
     class Maghrebemergent
       class << self
         include AbstractController::Rendering
-        include Articles::Crawling::Crawlingmethods
+       # include Articles::Crawling::Crawlingmethods
         def get_articles_maghrebemergent(url_media_array, media)
           articles_url_maghrebemergent = []
           count = 0
@@ -96,6 +96,138 @@ module Articles
           end
           count
         end
+
+        private 
+        def change_translate_date(d)
+            d.split.map do |m|
+              case m.downcase
+              when 'Janvier,'.downcase
+                'January'
+              when 'Février,'.downcase
+                'February'
+              when 'Mars,'.downcase
+                'March'
+              when 'Avril,'.downcase
+                'April'
+              when 'Mai,'.downcase
+                'May'
+              when 'Juin,'.downcase
+                'June'
+              when 'Juillet,'.downcase
+                'July'
+              when 'Octobre,'.downcase
+                'October'
+              when 'Novembre,'.downcase
+                'November'
+              when 'Décembre,'.downcase
+                'December'
+              when 'Septembre,'.downcase
+                'September'
+              when 'août,'.downcase
+                'August'
+              when 'Janvier'.downcase
+                'January'
+              when 'Février'.downcase
+                'February'
+              when 'Mars'.downcase
+                'March'
+              when 'Avril'.downcase
+                'April'
+              when 'Mai'.downcase
+                'May'
+              when 'Juin'.downcase
+                'June'
+              when 'Juillet'.downcase
+                'July'
+              when 'Octobre'.downcase
+                'October'
+              when 'Novembre'.downcase
+                'November'
+              when 'Décembre'.downcase
+                'December'
+              when 'Septembre'.downcase
+                'September'
+              when 'août'.downcase
+                'August'
+  
+              when 'جانفي'.downcase
+                'January'
+              when 'فيفري'.downcase
+                'February'
+              when 'مارس'.downcase
+                'March'
+              when 'افريل'.downcase
+                'April'
+              when 'ماي'.downcase
+                'May'
+              when 'جوان'.downcase
+                'June'
+              when 'جويلية'.downcase
+                'July'
+              when 'جولية'.downcase
+                'July'
+              when 'أكتوبر'.downcase
+                'October'
+              when 'نوفمبر'.downcase
+                'November'
+              when 'ديسمبر'.downcase
+                'December'
+              when 'سبتمبر'.downcase
+                'September'
+              when 'اوت'.downcase
+                'August'
+  
+              when 'جانفي،'.downcase
+                'January'
+              when 'فيفري،'.downcase
+                'February'
+              when 'مارس،'.downcase
+                'March'
+              when 'افريل،'.downcase
+                'April'
+              when 'ماي،'.downcase
+                'May'
+              when 'جوان،'.downcase
+                'June'
+              when 'جويلية،'.downcase
+                'July'
+              when 'جولية،'.downcase
+                'July'
+              when 'أكتوبر،'.downcase
+                'October'
+              when 'نوفمبر،'.downcase
+                'November'
+              when 'نونمبر،'.downcase
+                'November'
+              when 'ديسمبر،'.downcase
+                'December'
+              when 'سبتمبر،'.downcase
+                'September'
+              when 'اوت،'.downcase
+                'August'
+  
+              when 'يناير'.downcase
+                'January'
+              when 'فبراير'.downcase
+                'February'
+              when 'ابريل'.downcase
+                'April'
+              when 'أبريل'.downcase
+                'April'
+              when 'مايو'.downcase
+                'May'
+              when 'يونيو'.downcase
+                'June'
+              when 'يوليو'.downcase
+                'July'
+              when 'أغسطس'.downcase
+                'August'
+  
+              else
+                m
+              end
+            end.join(' ')
+          end
       end
     end
   end
