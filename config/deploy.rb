c29e0ee6209c32bd8ea0f682c392470d15107b89
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-08-28 02:12:39 +0200

    add credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :stage, :production
 # Deploy to the user's home directory
 set :deploy_to, "/var/www/html/#{fetch :application}"
 set :whenever_environment, -> { fetch(:stage) }
-set :linked_files, %w[config/master.key config/credentials.yml.enc]
+set :linked_files, %w[config/master.key]
 # set :whenever_identifier, ->{ "/var/www/html/#{fetch(:application)}_#{fetch(:stage)}" }
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system',
        'public/uploads'
