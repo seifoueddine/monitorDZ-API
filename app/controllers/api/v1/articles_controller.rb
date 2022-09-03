ab8b0c57c3537698f374aae5c0d68d2055a3e67d
Seif Eddine NOUARA <nouaraseifeddine@gmail.com>
2022-09-03 15:08:09 +0200

    change token_lifespan 24h

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -21,7 +21,7 @@ module Api
   module V1
     # crawling articles
     class ArticlesController < ::ApplicationController
-      # before_action :authenticate_user! , except: :pdf_export
+      before_action :authenticate_user! , except: :pdf_export
       before_action :set_article, only: %i[show update destroy]
       require 'nokogiri'
       require 'open-uri'
