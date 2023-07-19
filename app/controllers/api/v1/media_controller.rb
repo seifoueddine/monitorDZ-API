0e4f846a51ebe36582381481e6cbaae32fbb37ed
seif <nouaraseifeddine@gmail.com>
2023-07-19 20:54:47 +0200

    remove require medium

--- app/controllers/api/v1/media_controller.rb
+++ app/controllers/api/v1/media_controller.rb
@@ -63,7 +63,9 @@ module Api
 
       # Only allow a trusted parameter "white list" through.
       def medium_params
-        params.require(:medium).permit(:name, :media_type, :orientation, :last_article,
+        # params.require(:medium).permit(:name, :media_type, :orientation, :last_article,
+        #               :url_crawling, :avatar, :language, :zone, :tag_status)
+           params.permit(:name, :media_type, :orientation, :last_article,
                       :url_crawling, :avatar, :language, :zone, :tag_status)
       end
     end
