142a0d8acc4d34d8b9bba466f42db4552ea1abf3
seif <nouaraseifeddine@gmail.com>
2023-08-27 15:57:55 +0200

    refacto ennahar code

--- lib/articles/crawling/ennahar.rb
+++ lib/articles/crawling/ennahar.rb
@@ -1,96 +1,116 @@
 # frozen_string_literal: true
 
 module Articles
-  # crawling files
   module Crawling
-    # methode to get Ennahar articles
     class Ennahar
+      USER_AGENT = 'ruby/2.6.5'
+
       class << self
         include AbstractController::Rendering
+
         def get_articles_ennahar(url_media_array, media)
-          count = 0
+          articles_urls = fetch_all_article_urls(url_media_array)
+          existing_articles = fetch_existing_articles(media, articles_urls[:dates])
+
+          articles_urls[:urls] -= existing_articles
+          count_new_articles(articles_urls[:urls], media)
+        end
+
+        private
+
+        def fetch_all_article_urls(url_media_array)
           articles_url_ennahar = []
           last_dates = []
-          url_media_array.map do |url|
-            begin
-              doc = Nokogiri::HTML(URI.open(url, 'User-Agent' => 'ruby/2.6.5'))
-            rescue OpenURI::HTTPError => e
-              puts "Can't access #{url}"
-              puts e.message
-              puts
-              next
-            end
 
-            doc.css('h2.card__title.x-middle a').map do |link|
-              articles_url_ennahar << link['href']
-            end
-            doc.css('time').map do |date|
-              last_dates << date['datetime']
-            end
+          url_media_array.each do |url|
+            doc = fetch_document(url)
+            next unless doc
+
+            articles_url_ennahar.concat(fetch_links_from_doc(doc))
+            last_dates.concat(fetch_dates_from_doc(doc))
           end
-          last_dates = last_dates.map { |d| d.to_datetime.change({ hour: 0, min: 0, sec: 0 }) + (1.0 / 24) }
-          articles_url_ennahar = articles_url_ennahar.reject(&:nil?)
-          last_dates = last_dates.uniq
-          last_articles = Article.where(medium_id: media.id).where(date_published: last_dates)
-          list_articles_url = []
-          last_articles.map do |article|
-            list_articles_url << article.url_article
-          end
-          articles_url_ennahar_after_check = articles_url_ennahar - list_articles_url
-          articles_url_ennahar_after_check.map do |link|
-            begin
-              article = Nokogiri::HTML(URI.open(link, 'User-Agent' => 'ruby/2.6.5'))
-            rescue OpenURI::HTTPError => e
-              puts "Can't access #{link}"
-              puts e.message
-              puts
-              next
-            end
 
-            new_article = Article.new
-            new_article.url_article = link
-            new_article.medium_id = media.id
-            new_article.language = media.language
-            new_article.category_article = article.css('div.sgb1__acat a').text
-            new_article.title = article.css('h1.sgb1__attl').text
-            # new_article.author = article.css('div.article-head__author div em a').text
+          {
+            urls: articles_url_ennahar.compact,
+            dates: last_dates.map { |d| d.to_datetime.change(hour: 0, min: 0, sec: 0) + (1.0 / 24) }.uniq
+          }
+        end
 
-            author_exist = Author.where(['lower(name) like ? ',
-                                         article.at('div.sgb1__aath a').text.downcase])
+        def fetch_document(url)
+          Nokogiri::HTML(URI.open(url, 'User-Agent' => USER_AGENT))
+        rescue OpenURI::HTTPError => e
+          puts "Can't access #{url}: #{e.message}"
+          nil
+        end
 
-            new_author = Author.new
-            if author_exist.count.zero?
+        def fetch_links_from_doc(doc)
+          doc.css('h2.card__title.x-middle a').map { |link| link['href'] }
+        end
 
-              new_author.name = article.at('div.sgb1__aath a').text
-              new_author.medium_id = media.id
-              new_author.save!
-              new_article.author_id = new_author.id
-            else
-              new_article.author_id = author_exist.first.id
+        def fetch_dates_from_doc(doc)
+          doc.css('time').map { |date| date['datetime'] }
+        end
 
-            end
-            new_article.body = article.css('div.artx').inner_html
-            new_article.body = new_article.body.gsub(/<img[^>]*>/, '')
-            new_article.date_published = article.at('time[datetime]')['datetime'].to_datetime.change({ hour: 0, min: 0,
-                                                                                                       sec: 0 }) + (1.0 / 24)
-            url_array = article.css('figure.sgb1__afig div.sgb1__afmg img').map { |link| link['data-src'] }
-            new_article.url_image = url_array[0]
-            begin
-              new_article.image = Down.download(url_array[0]) if url_array[0].present?
-            rescue Down::Error => e
-              puts "Can't download this image #{url_array[0]}"
-              puts e.message
-              puts
-              new_article.image = nil
-            end
-            # tags_array = article.css('div.article-core__tags a').map(&:text)
-            # new_article.media_tags = tags_array.join(',')
-            new_article.status = 'pending'
-            new_article.save!
-            # tag_check_and_save(tags_array)
-            count += 1 if new_article.save
+        def fetch_existing_articles(media, dates)
+          Article.where(medium_id: media.id, date_published: dates).pluck(:url_article)
+        end
+
+        def count_new_articles(articles_urls, media)
+          count = 0
+          articles_urls.each do |url|
+            article_doc = fetch_document(url)
+            next unless article_doc
+
+            new_article = build_article(article_doc, url, media)
+            count += 1 if new_article&.save
           end
           count
+        end
+
+        def build_article(doc, url, media)
+          new_article = Article.new
+          new_article.attributes = {
+            url_article: url,
+            medium_id: media.id,
+            language: media.language,
+            category_article: doc.css('div.sgb1__acat a').text,
+            title: doc.css('h1.sgb1__attl').text,
+            body: sanitize_body(doc.css('div.artx').inner_html),
+            date_published: doc.at('time[datetime]')['datetime'].to_datetime.change(hour: 0, min: 0,
+                                                                                    sec: 0) + (1.0 / 24),
+            url_image: doc.css('figure.sgb1__afig div.sgb1__afmg img').map { |link| link['data-src'] }.first,
+            status: 'pending'
+          }
+
+          assign_author(new_article, doc, media)
+          assign_image(new_article)
+
+          new_article
+        end
+
+        def assign_author(article, doc, media)
+          author_name = doc.at('div.sgb1__aath a').text.downcase
+          author = Author.find_or_initialize_by(name: author_name)
+          if author.new_record?
+            author.medium_id = media.id
+            author.save!
+          end
+          article.author = author
+        end
+
+        def assign_image(article)
+          return unless article.url_image
+
+          begin
+            article.image = Down.download(article.url_image)
+          rescue Down::Error => e
+            puts "Can't download this image #{article.url_image}: #{e.message}"
+            article.image = nil
+          end
+        end
+
+        def sanitize_body(body)
+          body.gsub(/<img[^>]*>/, '')
         end
       end
     end
